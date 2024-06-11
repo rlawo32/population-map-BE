11f39632606fac0f2198ba604d40364446367b40
rlawo <rlawo32@naver.com>
2024-06-12 00:21:41 +0900

    first commit

---
+++ src/main/java/com/pm/pmproject/PopulationMapBeApplication.java
@@ -0,0 +1,13 @@
+package com.pm.pmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationMapBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationMapBeApplication.class, args);
+	}
+
+}
