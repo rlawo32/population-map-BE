5e0a06dfb8a2fb0df9c41c1d05e8ba2af8d44c45
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-06-12 10:32:13 +0900

    Update PopulationJan0s.java

--- src/main/java/com/pm/pmproject/jpa/domain/populationJan/PopulationJan0s.java
+++ src/main/java/com/pm/pmproject/jpa/domain/populationJan/PopulationJan0s.java
@@ -17,33 +17,64 @@ public class PopulationJan0s {
     @Column(name = "admin_code")
     private Long adminCode;
 
-    @Column(name = "pop_age_0")
-    private Long popAge0;
+    @Column(name = "pop_age_m_0")
+    private Long popAgeM0;
 
-    @Column(name = "pop_age_1")
-    private Long popAge1;
+    @Column(name = "pop_age_w_0")
+    private Long popAgeW0;
 
-    @Column(name = "pop_age_2")
-    private Long popAge2;
+    @Column(name = "pop_age_m_1")
+    private Long popAgeM1;
 
-    @Column(name = "pop_age_3")
-    private Long popAge3;
+    @Column(name = "pop_age_w_1")
+    private Long popAgeW1;
+    
+    @Column(name = "pop_age_m_2")
+    private Long popAgeM2;
 
-    @Column(name = "pop_age_4")
-    private Long popAge4;
+    @Column(name = "pop_age_w_2")
+    private Long popAgeW2;
+ 
+    @Column(name = "pop_age_m_3")
+    private Long popAgeM3;
 
-    @Column(name = "pop_age_5")
-    private Long popAge5;
+    @Column(name = "pop_age_w_3")
+    private Long popAgeW3;
+ 
+    @Column(name = "pop_age_m_4")
+    private Long popAgeM4;
 
-    @Column(name = "pop_age_6")
-    private Long popAge6;
+    @Column(name = "pop_age_w_4")
+    private Long popAgeW4;
+ 
+    @Column(name = "pop_age_m_5")
+    private Long popAgeM5;
 
-    @Column(name = "pop_age_7")
-    private Long popAge7;
+    @Column(name = "pop_age_w_5")
+    private Long popAgeW5;
+ 
+    @Column(name = "pop_age_m_6")
+    private Long popAgeM6;
 
-    @Column(name = "pop_age_8")
-    private Long popAge8;
+    @Column(name = "pop_age_w_6")
+    private Long popAgeW6;
+ 
+    @Column(name = "pop_age_m_7")
+    private Long popAgeM7;
 
-    @Column(name = "pop_age_9")
-    private Long popAge9;
+    @Column(name = "pop_age_w_7")
+    private Long popAgeW7;
+ 
+    @Column(name = "pop_age_m_8")
+    private Long popAgeM8;
+
+    @Column(name = "pop_age_w_8")
+    private Long popAgeW8;
+ 
+    @Column(name = "pop_age_m_9")
+    private Long popAgeM9;
+    
+    @Column(name = "pop_age_w_9")
+    private Long popAgeW9;
+ 
 }
