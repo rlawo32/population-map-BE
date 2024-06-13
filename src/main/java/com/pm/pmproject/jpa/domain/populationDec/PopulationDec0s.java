d9207f216c87c6fd5e76f066f662eb41171e856d
rlawo <rlawo32@naver.com>
2024-06-14 02:35:57 +0900

    [PopulationMap-project][BE] <JPA> Entity_작업_12월_20240614 (1)

---
+++ src/main/java/com/pm/pmproject/jpa/domain/populationDec/PopulationDec0s.java
@@ -0,0 +1,80 @@
+package com.pm.pmproject.jpa.domain.populationDec;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "PopulationDec0s")
+public class PopulationDec0s {
+
+    @Id
+    @Column(name = "admin_code")
+    private Long adminCode;
+
+    @Column(name = "pop_age_m_0")
+    private Long popAgeM0;
+
+    @Column(name = "pop_age_w_0")
+    private Long popAgeW0;
+
+    @Column(name = "pop_age_m_1")
+    private Long popAgeM1;
+
+    @Column(name = "pop_age_w_1")
+    private Long popAgeW1;
+    
+    @Column(name = "pop_age_m_2")
+    private Long popAgeM2;
+
+    @Column(name = "pop_age_w_2")
+    private Long popAgeW2;
+ 
+    @Column(name = "pop_age_m_3")
+    private Long popAgeM3;
+
+    @Column(name = "pop_age_w_3")
+    private Long popAgeW3;
+ 
+    @Column(name = "pop_age_m_4")
+    private Long popAgeM4;
+
+    @Column(name = "pop_age_w_4")
+    private Long popAgeW4;
+ 
+    @Column(name = "pop_age_m_5")
+    private Long popAgeM5;
+
+    @Column(name = "pop_age_w_5")
+    private Long popAgeW5;
+ 
+    @Column(name = "pop_age_m_6")
+    private Long popAgeM6;
+
+    @Column(name = "pop_age_w_6")
+    private Long popAgeW6;
+ 
+    @Column(name = "pop_age_m_7")
+    private Long popAgeM7;
+
+    @Column(name = "pop_age_w_7")
+    private Long popAgeW7;
+ 
+    @Column(name = "pop_age_m_8")
+    private Long popAgeM8;
+
+    @Column(name = "pop_age_w_8")
+    private Long popAgeW8;
+ 
+    @Column(name = "pop_age_m_9")
+    private Long popAgeM9;
+    
+    @Column(name = "pop_age_w_9")
+    private Long popAgeW9;
+ 
+}
---
+++ src/main/java/com/pm/pmproject/jpa/domain/populationDec/PopulationDec10s.java
@@ -0,0 +1,79 @@
+package com.pm.pmproject.jpa.domain.populationDec;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "PopulationDec10s")
+public class PopulationDec10s {
+
+    @Id
+    @Column(name = "admin_code")
+    private Long adminCode;
+
+    @Column(name = "pop_age_m_10")
+    private Long popAgeM10;
+
+    @Column(name = "pop_age_w_10")
+    private Long popAgeW10;
+
+    @Column(name = "pop_age_m_11")
+    private Long popAgeM11;
+
+    @Column(name = "pop_age_w_11")
+    private Long popAgeW11;
+
+    @Column(name = "pop_age_m_12")
+    private Long popAgeM12;
+
+    @Column(name = "pop_age_w_12")
+    private Long popAgeW12;
+
+    @Column(name = "pop_age_m_13")
+    private Long popAgeM13;
+
+    @Column(name = "pop_age_w_13")
+    private Long popAgeW13;
+
+    @Column(name = "pop_age_m_14")
+    private Long popAgeM14;
+
+    @Column(name = "pop_age_w_14")
+    private Long popAgeW14;
+
+    @Column(name = "pop_age_m_15")
+    private Long popAgeM15;
+
+    @Column(name = "pop_age_w_15")
+    private Long popAgeW15;
+
+    @Column(name = "pop_age_m_16")
+    private Long popAgeM16;
+
+    @Column(name = "pop_age_w_16")
+    private Long popAgeW16;
+
+    @Column(name = "pop_age_m_17")
+    private Long popAgeM17;
+
+    @Column(name = "pop_age_w_17")
+    private Long popAgeW17;
+
+    @Column(name = "pop_age_m_18")
+    private Long popAgeM18;
+
+    @Column(name = "pop_age_w_18")
+    private Long popAgeW18;
+
+    @Column(name = "pop_age_m_19")
+    private Long popAgeM19;
+
+    @Column(name = "pop_age_w_19")
+    private Long popAgeW19;
+}
