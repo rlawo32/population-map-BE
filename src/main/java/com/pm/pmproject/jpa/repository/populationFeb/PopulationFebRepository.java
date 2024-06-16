3687efbc550eb3755eb9b6c96057b573fac82c47
rlawo <rlawo32@naver.com>
2024-06-16 14:53:21 +0900

    [PopulationMap-project][BE] <JPA> Repo_작업_2월_20240615 (1)

---
+++ src/main/java/com/pm/pmproject/jpa/repository/populationFeb/PopulationFebRepository.java
@@ -0,0 +1,8 @@
+package com.pm.pmproject.jpa.repository.populationFeb;
+
+import com.pm.pmproject.jpa.domain.populationFeb.PopulationFeb;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopulationFebRepository extends JpaRepository<PopulationFeb, Long> {
+
+}
