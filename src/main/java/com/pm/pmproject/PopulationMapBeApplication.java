fd05a392de673d6d5d1d75713d5d7c9b22e02f5d
rlawo <rlawo32@naver.com>
2024-06-19 01:48:37 +0900

    [PopulationMap-project][BE] <API> API통신_작업_20240619 (4)

--- src/main/java/com/pm/pmproject/PopulationMapBeApplication.java
+++ src/main/java/com/pm/pmproject/PopulationMapBeApplication.java
@@ -10,10 +10,10 @@ import java.util.TimeZone;
 @EnableScheduling
 @SpringBootApplication
 public class PopulationMapBeApplication {
-	@PostConstruct //프로젝트 실행 시, 처음 한번만 실행
-	public void setTimeZone(){
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
+//	@PostConstruct //프로젝트 실행 시, 처음 한번만 실행
+//	public void setTimeZone(){
+//		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(PopulationMapBeApplication.class, args);
 	}
