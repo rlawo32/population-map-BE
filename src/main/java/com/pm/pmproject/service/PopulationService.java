91e867d9fa43bc402a86d866ad2d425c81e0b6f2
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-06-19 17:26:25 +0900

    [PopulationMap-project][BE] <API> API통신_LIST변환_작업_20240619 (1)

--- src/main/java/com/pm/pmproject/service/PopulationService.java
+++ src/main/java/com/pm/pmproject/service/PopulationService.java
@@ -99,6 +99,8 @@ public class PopulationService {
             List<Population90sDto> list9 = Arrays.asList(objectMapper.treeToValue(node2, Population90sDto[].class));
             List<Population1000sDto> list10 = Arrays.asList(objectMapper.treeToValue(node2, Population1000sDto[].class));
             list.forEach(populationResultDto -> System.out.println(populationResultDto.getNameTown()));
+            list4.forEach(population40sDto -> System.out.println(population40sDto.adminCode()) System.out.println(population40sDto.getAgeM42()));
+            list9.forEach(population90sDto -> System.out.println(population90sDto.adminCode()) System.out.println(population90sDto.getAgeM94()));
 
         } catch(Exception e) {
             e.printStackTrace();
