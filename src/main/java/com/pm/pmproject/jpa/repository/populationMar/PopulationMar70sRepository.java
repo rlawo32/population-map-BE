8c1ae624099c9b20119109f193664c7c1ecee7a9
rlawo <rlawo32@naver.com>
2024-06-24 01:52:27 +0900

    [PopulationMap-project][BE] <JPA> BATCH_INSERT_작업(3월)_20240624 (4)

---
+++ src/main/java/com/pm/pmproject/jpa/repository/populationMar/PopulationMar70sRepository.java
@@ -0,0 +1,60 @@
+package com.pm.pmproject.jpa.repository.populationMar;
+
+import com.pm.pmproject.jpa.domain.populationMar.PopulationMar70s;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class PopulationMar70sRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PopulationMar70sRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void batchInsert(List<PopulationMar70s> list) {
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO population_mar_70s (admin_code, pop_age_m_70, pop_age_w_70, pop_age_m_71, " +
+                        "pop_age_w_71, pop_age_m_72, pop_age_w_72, pop_age_m_73, pop_age_w_73, pop_age_m_74, " +
+                        "pop_age_w_74, pop_age_m_75, pop_age_w_75, pop_age_m_76, pop_age_w_76, pop_age_m_77, " +
+                        "pop_age_w_77, pop_age_m_78, pop_age_w_78, pop_age_m_79, pop_age_w_79) " +
+                        "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setLong(1, list.get(i).getAdminCode());
+                        ps.setLong(2, list.get(i).getPopAgeM70());
+                        ps.setLong(3, list.get(i).getPopAgeW70());
+                        ps.setLong(4, list.get(i).getPopAgeM71());
+                        ps.setLong(5, list.get(i).getPopAgeW71());
+                        ps.setLong(6, list.get(i).getPopAgeM72());
+                        ps.setLong(7, list.get(i).getPopAgeW72());
+                        ps.setLong(8, list.get(i).getPopAgeM73());
+                        ps.setLong(9, list.get(i).getPopAgeW73());
+                        ps.setLong(10, list.get(i).getPopAgeM74());
+                        ps.setLong(11, list.get(i).getPopAgeW74());
+                        ps.setLong(12, list.get(i).getPopAgeM75());
+                        ps.setLong(13, list.get(i).getPopAgeW75());
+                        ps.setLong(14, list.get(i).getPopAgeM76());
+                        ps.setLong(15, list.get(i).getPopAgeW76());
+                        ps.setLong(16, list.get(i).getPopAgeM77());
+                        ps.setLong(17, list.get(i).getPopAgeW77());
+                        ps.setLong(18, list.get(i).getPopAgeM78());
+                        ps.setLong(19, list.get(i).getPopAgeW78());
+                        ps.setLong(20, list.get(i).getPopAgeM79());
+                        ps.setLong(21, list.get(i).getPopAgeW79());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return list.size();
+                    }
+                });
+    }
+}
