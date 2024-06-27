85d640dddf0cc93546498b434c3aad0ab32cea72
rlawo <rlawo32@naver.com>
2024-06-27 22:12:16 +0900

    [PopulationMap-project][BE] <JPA> BATCH_INSERT_작업(10월)_20240627 (5)

---
+++ src/main/java/com/pm/pmproject/jpa/repository/populationOct/PopulationOct100sRepository.java
@@ -0,0 +1,60 @@
+package com.pm.pmproject.jpa.repository.populationOct;
+
+import com.pm.pmproject.jpa.domain.populationOct.PopulationOct100s;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class PopulationOct100sRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PopulationOct100sRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void batchInsert(List<PopulationOct100s> list) {
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO population_oct_100s (admin_code, pop_age_m_100, pop_age_w_100, pop_age_m_101, " +
+                        "pop_age_w_101, pop_age_m_102, pop_age_w_102, pop_age_m_103, pop_age_w_103, pop_age_m_104, " +
+                        "pop_age_w_104, pop_age_m_105, pop_age_w_105, pop_age_m_106, pop_age_w_106, pop_age_m_107, " +
+                        "pop_age_w_107, pop_age_m_108, pop_age_w_108, pop_age_m_109, pop_age_w_109) " +
+                        "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setLong(1, list.get(i).getAdminCode());
+                        ps.setLong(2, list.get(i).getPopAgeM100());
+                        ps.setLong(3, list.get(i).getPopAgeW100());
+                        ps.setLong(4, list.get(i).getPopAgeM101());
+                        ps.setLong(5, list.get(i).getPopAgeW101());
+                        ps.setLong(6, list.get(i).getPopAgeM102());
+                        ps.setLong(7, list.get(i).getPopAgeW102());
+                        ps.setLong(8, list.get(i).getPopAgeM103());
+                        ps.setLong(9, list.get(i).getPopAgeW103());
+                        ps.setLong(10, list.get(i).getPopAgeM104());
+                        ps.setLong(11, list.get(i).getPopAgeW104());
+                        ps.setLong(12, list.get(i).getPopAgeM105());
+                        ps.setLong(13, list.get(i).getPopAgeW105());
+                        ps.setLong(14, list.get(i).getPopAgeM106());
+                        ps.setLong(15, list.get(i).getPopAgeW106());
+                        ps.setLong(16, list.get(i).getPopAgeM107());
+                        ps.setLong(17, list.get(i).getPopAgeW107());
+                        ps.setLong(18, list.get(i).getPopAgeM108());
+                        ps.setLong(19, list.get(i).getPopAgeW108());
+                        ps.setLong(20, list.get(i).getPopAgeM109());
+                        ps.setLong(21, list.get(i).getPopAgeW109());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return list.size();
+                    }
+                });
+    }
+}
--- src/main/java/com/pm/pmproject/jpa/repository/populationOct/PopulationOctRepository.java
+++ src/main/java/com/pm/pmproject/jpa/repository/populationOct/PopulationOctRepository.java
@@ -1,8 +1,45 @@
 package com.pm.pmproject.jpa.repository.populationOct;
 
 import com.pm.pmproject.jpa.domain.populationOct.PopulationOct;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-public interface PopulationOctRepository extends JpaRepository<PopulationOct, Long> {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
+@Repository
+public class PopulationOctRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PopulationOctRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void batchInsert(List<PopulationOct> list) {
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO population_oct (admin_code, standard_date, name_city, " +
+                        "name_ward, name_town, pop_total, pop_m_total, pop_w_total) " +
+                        "VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setLong(1, list.get(i).getAdminCode());
+                        ps.setString(2, list.get(i).getStandardDate());
+                        ps.setString(3, list.get(i).getNameCity());
+                        ps.setString(4, list.get(i).getNameWard());
+                        ps.setString(5, list.get(i).getNameTown());
+                        ps.setLong(6, list.get(i).getPopTotal());
+                        ps.setLong(7, list.get(i).getPopMTotal());
+                        ps.setLong(8, list.get(i).getPopWTotal());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return list.size();
+                    }
+                });
+    }
 }
