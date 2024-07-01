da7927b8a5929125c4ac5098c413b3d1aa8047ce
rlawo <rlawo32@naver.com>
2024-07-02 00:32:06 +0900

    [PopulationMap-project][BE] <JDBC> BATCH_INSERT_재작업(3월)_20240702 (3)

--- src/main/java/com/pm/pmproject/jpa/domain/populationMar/PopulationMar70s.java
+++ src/main/java/com/pm/pmproject/jpa/domain/populationMar/PopulationMar70s.java
@@ -1,23 +1,30 @@
 package com.pm.pmproject.jpa.domain.populationMar;
 
-import com.pm.pmproject.dto.Population70sDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Persistable;
 
 @NoArgsConstructor
 @Entity
 @Getter
 @Table(name = "PopulationMar_70s")
-public class PopulationMar70s implements Persistable<Long> {
+public class PopulationMar70s {
 
     @Id
     @Column(name = "admin_code")
     private Long adminCode;
+
+    @Column(name = "pop_age_total")
+    private Long popAgeTotal;
+
+    @Column(name = "pop_age_m_total")
+    private Long popAgeMTotal;
+
+    @Column(name = "pop_age_w_total")
+    private Long popAgeWTotal;
 
     @Column(name = "pop_age_m_70")
     private Long popAgeM70;
@@ -78,38 +85,4 @@ public class PopulationMar70s implements Persistable<Long> {
 
     @Column(name = "pop_age_w_79")
     private Long popAgeW79;
-
-    @Override
-    public boolean isNew() {
-        return true;
-    }
-
-    @Override
-    public Long getId() {
-        return this.adminCode;
-    }
-
-    public PopulationMar70s(Population70sDto population70sDto) {
-        this.adminCode = population70sDto.getAdminCode();
-        this.popAgeM70 = population70sDto.getPopAgeM70();
-        this.popAgeW70 = population70sDto.getPopAgeW70();
-        this.popAgeM71 = population70sDto.getPopAgeM71();
-        this.popAgeW71 = population70sDto.getPopAgeW71();
-        this.popAgeM72 = population70sDto.getPopAgeM72();
-        this.popAgeW72 = population70sDto.getPopAgeW72();
-        this.popAgeM73 = population70sDto.getPopAgeM73();
-        this.popAgeW73 = population70sDto.getPopAgeW73();
-        this.popAgeM74 = population70sDto.getPopAgeM74();
-        this.popAgeW74 = population70sDto.getPopAgeW74();
-        this.popAgeM75 = population70sDto.getPopAgeM75();
-        this.popAgeW75 = population70sDto.getPopAgeW75();
-        this.popAgeM76 = population70sDto.getPopAgeM76();
-        this.popAgeW76 = population70sDto.getPopAgeW76();
-        this.popAgeM77 = population70sDto.getPopAgeM77();
-        this.popAgeW77 = population70sDto.getPopAgeW77();
-        this.popAgeM78 = population70sDto.getPopAgeM78();
-        this.popAgeW78 = population70sDto.getPopAgeW78();
-        this.popAgeM79 = population70sDto.getPopAgeM79();
-        this.popAgeW79 = population70sDto.getPopAgeW79();
-    }
 }
