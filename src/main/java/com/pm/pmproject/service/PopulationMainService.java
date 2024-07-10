c88c9f9107f26cfd8f85c11214ca17a29ad1eb36
rlawo <rlawo32@naver.com>
2024-07-10 22:52:51 +0900

    [PopulationMap-project][BE] <API> SWAGGER_작업_20240710 (6)

--- src/main/java/com/pm/pmproject/service/PopulationMainService.java
+++ src/main/java/com/pm/pmproject/service/PopulationMainService.java
@@ -12,10 +12,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -45,34 +52,118 @@ public class PopulationMainService {
 //    @Scheduled(cron = "0 0 0 * * ?") // 매일 24시
 //    @Scheduled(cron = "0 0 0/6 * * *") // 6시간마다
     @Scheduled(cron = "0 0/1 * * * *") // 10분마다
-    public void schedulerPopulationUpdate() {
+    public void SchedulerMonthCheckWithSwagger() {
+        try {
+            // 1. URL 설정
+            StringBuilder urlBuilder = new StringBuilder("https://infuser.odcloud.kr/oas/docs?namespace=15097972/v1");
+
+            // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
+            urlBuilder.append("&" + URLEncoder.encode("serviceKey","UTF-8") + "=" + apiKey);
+
+            // 3. URI 생성
+            URI uri = URI.create(urlBuilder.toString());
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+
+            // 4. HttpClient 객체 선언
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            // 5. HttpClient URI, 메서드, Header, Timeout 선언 후 request 생성
+            HttpRequest req = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .header("Content-Type", "application/json;charset=utf-8")
+                    .timeout(Duration.ofSeconds(20000))
+                    .build();
+
+            // 6. HttpClient request 요청 후 response 값 반환
+            HttpResponse<String> res = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+
+            // 7. 통신 응답 코드 확인.
+            System.out.print("Swagger 통신 결과 : ");
+            if(res.statusCode() == 200) {
+                System.out.println("성공");
+            } else {
+                System.out.println("실패");
+            }
+            stopWatch.stop();
+            System.out.println("통신 시간 : " + stopWatch.getTotalTimeSeconds());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            JsonNode node1 = objectMapper.readTree(res.body());
+            JsonNode node2 = node1.findValue("paths");
+
+
+            System.out.println(node2);
+
+            List<JsonNode> node3 = node2.findValues("get");
+            System.out.println(node3.size());
+            System.out.print(node3.get(0).get("summary"));
+            System.out.println(node3.get(0).get("operationId"));
+            System.out.print(node3.get(1).get("summary"));
+            System.out.println(node3.get(1).get("operationId"));
+
+            LocalDateTime ldt = LocalDateTime.now().minusMonths(1);
+
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMM");
+            String formatDate = ldt.format(dtf);
+            System.out.println(formatDate);
+
+
+            System.out.println("DATA");
+
+            for(int n=0; n<node3.size(); n++) {
+                String summary = node3.get(n).get("summary").asText();
+
+                int p1 = summary.lastIndexOf("_");
+                String p2 = summary.substring(p1+1, p1+7);
+
+                System.out.println(p2);
+            }
+        } catch (UnsupportedEncodingException ex) {
+            System.out.println("Error Code [1]");
+        } catch (IOException ex) {
+            System.out.println("Error Code [2]");
+        } catch (InterruptedException ex) {
+            System.out.println("Error Code [3]");
+        }
+    }
+
+    private void SchedulerPopulationUpdate(String apiUrl) {
         try {
             for(int i=1; i<=1; i++) {
+                // 1. URL 설정 uddi:780a2373-bf11-4fb6-b3e4-ed4119571817
+                StringBuilder urlBuilder = new StringBuilder("https://api.odcloud.kr/api/15097972/v1/" + apiUrl);
 
-                // 1. URL 설정
-//                StringBuilder urlBuilder = new StringBuilder("https://api.odcloud.kr/api/15097972/v1/uddi:780a2373-bf11-4fb6-b3e4-ed4119571817");
-                StringBuilder urlBuilder = new StringBuilder("https://infuser.odcloud.kr/oas/docs?namespace=15097972/v1");
                 // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
-//                urlBuilder.append("?" + URLEncoder.encode("page","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
-//                urlBuilder.append("&" + URLEncoder.encode("perPage","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8"));
+                urlBuilder.append("?" + URLEncoder.encode("page","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
+                urlBuilder.append("&" + URLEncoder.encode("perPage","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
                 urlBuilder.append("&" + URLEncoder.encode("serviceKey","UTF-8") + "=" + apiKey);
+
                 // 3. URL 객체 생성.
                 URL url = new URL(urlBuilder.toString());
                 StopWatch stopWatch = new StopWatch();
                 stopWatch.start();
+
                 // 4. 요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성.
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
                 // 5. 통신을 위한 메소드 SET.
                 conn.setRequestMethod("GET");
+
                 // 6. 통신을 위한 Content-type SET.
                 conn.setRequestProperty("Content-type", "application/json");
+                conn.setConnectTimeout(20000);
+
                 // 7. 통신 응답 코드 확인.
-                System.out.print(i + "번째 통신 결과 : ");
+                System.out.print("Swagger 통신 결과 : ");
                 if(conn.getResponseCode() == 200) {
                     System.out.println("성공");
                 } else {
                     System.out.println("실패");
                 }
+
                 // 8. 전달받은 데이터를 BufferedReader 객체로 저장.
                 BufferedReader br;
                 if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
@@ -80,12 +171,14 @@ public class PopulationMainService {
                 } else {
                     br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
                 }
+
                 // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장.
                 StringBuilder sb = new StringBuilder();
                 String line;
                 while ((line = br.readLine()) != null) {
                     sb.append(line);
                 }
+
                 // 10. 객체 해제.
                 br.close();
                 conn.disconnect();
@@ -95,30 +188,7 @@ public class PopulationMainService {
                 ObjectMapper objectMapper = new ObjectMapper();
 
                 JsonNode node1 = objectMapper.readTree(sb.toString());
-                JsonNode node2 = node1.findValue("paths");
-
-                System.out.println(node2);
-
-                List<JsonNode> node3 = node2.findValues("get");
-                System.out.println(node3.size());
-                System.out.print(node3.get(0).get("summary"));
-                System.out.println(node3.get(0).get("operationId"));
-                System.out.print(node3.get(1).get("summary"));
-                System.out.println(node3.get(1).get("operationId"));
-
-                LocalDateTime ldt = LocalDateTime.now();
-
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMM");
-                String formatDate = ldt.format(dtf);
-
-                for(int n=0; n<node3.size(); n++) {
-                    String summary = node3.get(n).get("summary").asText();
-
-                    int p1 = summary.lastIndexOf("_");
-                    String p2 = summary.substring(p1+1, p1+7);
-
-                    System.out.println(p2);
-                }
+                JsonNode node2 = node1.findValue("data");
 
 //                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 //
@@ -146,8 +216,11 @@ public class PopulationMainService {
 //                populationMarService.populationMarUpdate(CommonRequestDto.setApiResultList(list, list0,
 //                        list1, list2, list3, list4, list5, list6, list7, list8, list9, list10));
             }
-        } catch(Exception e) {
-            e.printStackTrace();
+
+        } catch (UnsupportedEncodingException ex) {
+            System.out.println("Error Code [1]");
+        } catch (IOException ex) {
+            System.out.println("Error Code [2]");
         }
     }
 }
