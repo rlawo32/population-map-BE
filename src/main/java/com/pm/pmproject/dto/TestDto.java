39cd20b72d5845ece70f6bdcfcb43dc6e808ce02
rlawo <rlawo32@naver.com>
2024-07-14 04:12:49 +0900

    [PopulationMap-project][BE] <JPA> INSERT_정리_20240714 (4)

---
+++ src/main/java/com/pm/pmproject/dto/TestDto.java
@@ -0,0 +1,15 @@
+package com.pm.pmproject.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestDto {
+
+    private Long testValue1;
+    private String testValue2;
+}
---
+++ src/main/java/com/pm/pmproject/jpa/repository/test/TestRepository.java
@@ -0,0 +1,39 @@
+package com.pm.pmproject.jpa.repository.test;
+
+import com.pm.pmproject.dto.PopulationResultDto;
+import com.pm.pmproject.dto.TestDto;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class TestRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public TestRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void batchInsert(List<TestDto> list) {
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO test_table (test_value_1, test_value_2) " +
+                        "VALUES(?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setLong(1, list.get(i).getTestValue1());
+                        ps.setString(2, list.get(i).getTestValue2());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return list.size();
+                    }
+                });
+    }
+}
