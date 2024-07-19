fea4ceabf35f1bd5ed987c91802590140ec2c399
rlawo <rlawo32@naver.com>
2024-07-19 22:32:56 +0900

    [PopulationMap-project][BE] <MAIL> 메일전송_작업_20240719 (2)

--- src/main/java/com/pm/pmproject/PopulationMapBeApplication.java
+++ src/main/java/com/pm/pmproject/PopulationMapBeApplication.java
@@ -4,14 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 
 @EnableScheduling
 @SpringBootApplication
 public class PopulationMapBeApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(PopulationMapBeApplication.class, args);
-
-
 	}
 
 }
