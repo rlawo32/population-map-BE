bf909ac7ef019cce2a8eae2e7ad1ac44de42e964
rlawo <rlawo32@naver.com>
2024-08-13 23:22:47 +0900

    [PopulationMap-project][BE] <JDBC> SELECT_작업_20240813 (3)

--- src/main/java/com/pm/pmproject/jpa/repository/populationMay/PopulationMayRepository.java
+++ src/main/java/com/pm/pmproject/jpa/repository/populationMay/PopulationMayRepository.java
@@ -1,12 +1,17 @@
 package com.pm.pmproject.jpa.repository.populationMay;
 
 import com.pm.pmproject.dto.PopulationResultDto;
+import com.pm.pmproject.dto.TestDto;
+import com.pm.pmproject.jpa.domain.populationMay.PopulationMay;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -41,5 +46,28 @@ public class PopulationMayRepository {
                         return list.size();
                     }
                 });
+    }
+
+    public void batchSelect() {
+        Long result = 0L;
+
+        List<TestDto> list = jdbcTemplate.query("select * from test_table", new RowMapper<TestDto>() {
+            @Override
+            public TestDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                TestDto td = new TestDto();
+                td.setId(rs.getLong("id"));
+                td.setTestValue1(rs.getLong("test_value_1"));
+                td.setTestValue2(rs.getString("test_value_2"));
+                return td;
+            }
+        });
+
+        for(int i=0; i<list.size(); i++) {
+            System.out.print(list.get(i).getId() + "번째 테스트 : ");
+            System.out.print(list.get(i).getTestValue1() + " ");
+            System.out.println(list.get(i).getTestValue2());
+        }
+
+//        return result;
     }
 }
