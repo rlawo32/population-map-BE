3fb112cd544bad8370aa444c76084d18a6748f65
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-09-02 17:53:49 +0900

    [PopulationMap-project][BE] <JDBC> 지역별_TOTAL_작업_20240902 (17)

--- src/main/java/com/pm/pmproject/jpa/repository/populationMay/PopulationMay0sRepository.java
+++ src/main/java/com/pm/pmproject/jpa/repository/populationMay/PopulationMay0sRepository.java
@@ -112,6 +112,7 @@ public class PopulationMay0sRepository {
 
             if(!map.containsKey(nameCity)) {
                 map.put(nameCity, Arrays.asList(
+                    total, total_m, total_w,
                     sumAge0, list.get(i).getPopAgeM0(), list.get(i).getPopAgeW0(),
                     sumAge1, list.get(i).getPopAgeM1(), list.get(i).getPopAgeW1(),
                     sumAge2, list.get(i).getPopAgeM2(), list.get(i).getPopAgeW2(),
@@ -125,16 +126,17 @@ public class PopulationMay0sRepository {
                     ));
             } else {
                 map.put(nameCity, Arrays.asList(
-                    map.get(nameCity).get(0) + sumAge0, map.get(nameCity).get(1) + list.get(i).getPopAgeM0(), map.get(nameCity).get(2) + list.get(i).getPopAgeW0(),
-                    map.get(nameCity).get(3) + sumAge1, map.get(nameCity).get(4) + list.get(i).getPopAgeM1(), map.get(nameCity).get(5) + list.get(i).getPopAgeW1(),
-                    map.get(nameCity).get(6) + sumAge2, map.get(nameCity).get(7) + list.get(i).getPopAgeM2(), map.get(nameCity).get(8) + list.get(i).getPopAgeW2(),
-                    map.get(nameCity).get(9) + sumAge3, map.get(nameCity).get(10) + list.get(i).getPopAgeM3(), map.get(nameCity).get(11) + list.get(i).getPopAgeW3(),
-                    map.get(nameCity).get(12) + sumAge4, map.get(nameCity).get(13) + list.get(i).getPopAgeM4(), map.get(nameCity).get(14) + list.get(i).getPopAgeW4(),
-                    map.get(nameCity).get(15) + sumAge5, map.get(nameCity).get(16) + list.get(i).getPopAgeM5(), map.get(nameCity).get(17) + list.get(i).getPopAgeW5(),
-                    map.get(nameCity).get(18) + sumAge6, map.get(nameCity).get(19) + list.get(i).getPopAgeM6(), map.get(nameCity).get(20) + list.get(i).getPopAgeW6(),
-                    map.get(nameCity).get(21) + sumAge7, map.get(nameCity).get(22) + list.get(i).getPopAgeM7(), map.get(nameCity).get(23) + list.get(i).getPopAgeW7(),
-                    map.get(nameCity).get(24) + sumAge8, map.get(nameCity).get(25) + list.get(i).getPopAgeM8(), map.get(nameCity).get(26) + list.get(i).getPopAgeW8(),
-                    map.get(nameCity).get(27) + sumAge9, map.get(nameCity).get(28) + list.get(i).getPopAgeM9(), map.get(nameCity).get(29) + list.get(i).getPopAgeW9()
+                    map.get(nameCity).get(0) + total, map.get(nameCity).get(1) + total_m, map.get(nameCity).get(2) + total_w,
+                    map.get(nameCity).get(3) + sumAge0, map.get(nameCity).get(4) + list.get(i).getPopAgeM0(), map.get(nameCity).get(5) + list.get(i).getPopAgeW0(),
+                    map.get(nameCity).get(6) + sumAge1, map.get(nameCity).get(7) + list.get(i).getPopAgeM1(), map.get(nameCity).get(8) + list.get(i).getPopAgeW1(),
+                    map.get(nameCity).get(9) + sumAge2, map.get(nameCity).get(10) + list.get(i).getPopAgeM2(), map.get(nameCity).get(11) + list.get(i).getPopAgeW2(),
+                    map.get(nameCity).get(12) + sumAge3, map.get(nameCity).get(13) + list.get(i).getPopAgeM3(), map.get(nameCity).get(14) + list.get(i).getPopAgeW3(),
+                    map.get(nameCity).get(15) + sumAge4, map.get(nameCity).get(16) + list.get(i).getPopAgeM4(), map.get(nameCity).get(17) + list.get(i).getPopAgeW4(),
+                    map.get(nameCity).get(18) + sumAge5, map.get(nameCity).get(19) + list.get(i).getPopAgeM5(), map.get(nameCity).get(20) + list.get(i).getPopAgeW5(),
+                    map.get(nameCity).get(21) + sumAge6, map.get(nameCity).get(22) + list.get(i).getPopAgeM6(), map.get(nameCity).get(23) + list.get(i).getPopAgeW6(),
+                    map.get(nameCity).get(24) + sumAge7, map.get(nameCity).get(25) + list.get(i).getPopAgeM7(), map.get(nameCity).get(26) + list.get(i).getPopAgeW7(),
+                    map.get(nameCity).get(27) + sumAge8, map.get(nameCity).get(28) + list.get(i).getPopAgeM8(), map.get(nameCity).get(29) + list.get(i).getPopAgeW8(),
+                    map.get(nameCity).get(30) + sumAge9, map.get(nameCity).get(31) + list.get(i).getPopAgeM9(), map.get(nameCity).get(32) + list.get(i).getPopAgeW9()
                     ));
             }
 
@@ -147,6 +149,7 @@ public class PopulationMay0sRepository {
 		    
                 if(!map.containsKey(subCityWard1)) {
                     map.put(subCityWard1, Arrays.asList(
+                        total, total_m, total_w,
                         sumAge0, list.get(i).getPopAgeM0(), list.get(i).getPopAgeW0(),
                         sumAge1, list.get(i).getPopAgeM1(), list.get(i).getPopAgeW1(),
                         sumAge2, list.get(i).getPopAgeM2(), list.get(i).getPopAgeW2(),
@@ -160,16 +163,17 @@ public class PopulationMay0sRepository {
                         ));
                 } else {
                     map.put(subCityWard1, Arrays.asList(
-                        map.get(subCityWard1).get(0) + sumAge0, map.get(subCityWard1).get(1) + list.get(i).getPopAgeM0(), map.get(subCityWard1).get(2) + list.get(i).getPopAgeW0(),
-                        map.get(subCityWard1).get(3) + sumAge1, map.get(subCityWard1).get(4) + list.get(i).getPopAgeM1(), map.get(subCityWard1).get(5) + list.get(i).getPopAgeW1(),
-                        map.get(subCityWard1).get(6) + sumAge2, map.get(subCityWard1).get(7) + list.get(i).getPopAgeM2(), map.get(subCityWard1).get(8) + list.get(i).getPopAgeW2(),
-                        map.get(subCityWard1).get(9) + sumAge3, map.get(subCityWard1).get(10) + list.get(i).getPopAgeM3(), map.get(subCityWard1).get(11) + list.get(i).getPopAgeW3(),
-                        map.get(subCityWard1).get(12) + sumAge4, map.get(subCityWard1).get(13) + list.get(i).getPopAgeM4(), map.get(subCityWard1).get(14) + list.get(i).getPopAgeW4(),
-                        map.get(subCityWard1).get(15) + sumAge5, map.get(subCityWard1).get(16) + list.get(i).getPopAgeM5(), map.get(subCityWard1).get(17) + list.get(i).getPopAgeW5(),
-                        map.get(subCityWard1).get(18) + sumAge6, map.get(subCityWard1).get(19) + list.get(i).getPopAgeM6(), map.get(subCityWard1).get(20) + list.get(i).getPopAgeW6(),
-                        map.get(subCityWard1).get(21) + sumAge7, map.get(subCityWard1).get(22) + list.get(i).getPopAgeM7(), map.get(subCityWard1).get(23) + list.get(i).getPopAgeW7(),
-                        map.get(subCityWard1).get(24) + sumAge8, map.get(subCityWard1).get(25) + list.get(i).getPopAgeM8(), map.get(subCityWard1).get(26) + list.get(i).getPopAgeW8(),
-                        map.get(subCityWard1).get(27) + sumAge9, map.get(subCityWard1).get(28) + list.get(i).getPopAgeM9(), map.get(subCityWard1).get(29) + list.get(i).getPopAgeW9()
+                        map.get(subCityWard1).get(0) + total, map.get(subCityWard1).get(1) + total_m, map.get(subCityWard1).get(2) + total_w,
+                        map.get(subCityWard1).get(3) + sumAge0, map.get(subCityWard1).get(4) + list.get(i).getPopAgeM0(), map.get(subCityWard1).get(5) + list.get(i).getPopAgeW0(),
+                        map.get(subCityWard1).get(6) + sumAge1, map.get(subCityWard1).get(7) + list.get(i).getPopAgeM1(), map.get(subCityWard1).get(8) + list.get(i).getPopAgeW1(),
+                        map.get(subCityWard1).get(9) + sumAge2, map.get(subCityWard1).get(10) + list.get(i).getPopAgeM2(), map.get(subCityWard1).get(11) + list.get(i).getPopAgeW2(),
+                        map.get(subCityWard1).get(12) + sumAge3, map.get(subCityWard1).get(13) + list.get(i).getPopAgeM3(), map.get(subCityWard1).get(14) + list.get(i).getPopAgeW3(),
+                        map.get(subCityWard1).get(15) + sumAge4, map.get(subCityWard1).get(16) + list.get(i).getPopAgeM4(), map.get(subCityWard1).get(17) + list.get(i).getPopAgeW4(),
+                        map.get(subCityWard1).get(18) + sumAge5, map.get(subCityWard1).get(19) + list.get(i).getPopAgeM5(), map.get(subCityWard1).get(20) + list.get(i).getPopAgeW5(),
+                        map.get(subCityWard1).get(21) + sumAge6, map.get(subCityWard1).get(22) + list.get(i).getPopAgeM6(), map.get(subCityWard1).get(23) + list.get(i).getPopAgeW6(),
+                        map.get(subCityWard1).get(24) + sumAge7, map.get(subCityWard1).get(25) + list.get(i).getPopAgeM7(), map.get(subCityWard1).get(26) + list.get(i).getPopAgeW7(),
+                        map.get(subCityWard1).get(27) + sumAge8, map.get(subCityWard1).get(28) + list.get(i).getPopAgeM8(), map.get(subCityWard1).get(29) + list.get(i).getPopAgeW8(),
+                        map.get(subCityWard1).get(30) + sumAge9, map.get(subCityWard1).get(31) + list.get(i).getPopAgeM9(), map.get(subCityWard1).get(32) + list.get(i).getPopAgeW9()
                     	));
                 }
 
@@ -186,6 +190,7 @@ public class PopulationMay0sRepository {
             } else {
                 if(!map.containsKey(cityWard)) {
                     map.put(cityWard, Arrays.asList(
+                        total, total_m, total_w,
                         sumAge0, list.get(i).getPopAgeM0(), list.get(i).getPopAgeW0(),
                         sumAge1, list.get(i).getPopAgeM1(), list.get(i).getPopAgeW1(),
                         sumAge2, list.get(i).getPopAgeM2(), list.get(i).getPopAgeW2(),
@@ -199,16 +204,17 @@ public class PopulationMay0sRepository {
                         ));
                 } else {
                     map.put(cityWard, Arrays.asList(
-                        map.get(cityWard).get(0) + sumAge0, map.get(cityWard).get(1) + list.get(i).getPopAgeM0(), map.get(cityWard).get(2) + list.get(i).getPopAgeW0(),
-                        map.get(cityWard).get(3) + sumAge1, map.get(cityWard).get(4) + list.get(i).getPopAgeM1(), map.get(cityWard).get(5) + list.get(i).getPopAgeW1(),
-                        map.get(cityWard).get(6) + sumAge2, map.get(cityWard).get(7) + list.get(i).getPopAgeM2(), map.get(cityWard).get(8) + list.get(i).getPopAgeW2(),
-                        map.get(cityWard).get(9) + sumAge3, map.get(cityWard).get(10) + list.get(i).getPopAgeM3(), map.get(cityWard).get(11) + list.get(i).getPopAgeW3(),
-                        map.get(cityWard).get(12) + sumAge4, map.get(cityWard).get(13) + list.get(i).getPopAgeM4(), map.get(cityWard).get(14) + list.get(i).getPopAgeW4(),
-                        map.get(cityWard).get(15) + sumAge5, map.get(cityWard).get(16) + list.get(i).getPopAgeM5(), map.get(cityWard).get(17) + list.get(i).getPopAgeW5(),
-                        map.get(cityWard).get(18) + sumAge6, map.get(cityWard).get(19) + list.get(i).getPopAgeM6(), map.get(cityWard).get(20) + list.get(i).getPopAgeW6(),
-                        map.get(cityWard).get(21) + sumAge7, map.get(cityWard).get(22) + list.get(i).getPopAgeM7(), map.get(cityWard).get(23) + list.get(i).getPopAgeW7(),
-                        map.get(cityWard).get(24) + sumAge8, map.get(cityWard).get(25) + list.get(i).getPopAgeM8(), map.get(cityWard).get(26) + list.get(i).getPopAgeW8(),
-                        map.get(cityWard).get(27) + sumAge9, map.get(cityWard).get(28) + list.get(i).getPopAgeM9(), map.get(cityWard).get(29) + list.get(i).getPopAgeW9()
+                        map.get(cityWard).get(0) + total, map.get(cityWard).get(1) + total_m, map.get(cityWard).get(2) + total_w,
+                        map.get(cityWard).get(3) + sumAge0, map.get(cityWard).get(4) + list.get(i).getPopAgeM0(), map.get(cityWard).get(5) + list.get(i).getPopAgeW0(),
+                        map.get(cityWard).get(6) + sumAge1, map.get(cityWard).get(7) + list.get(i).getPopAgeM1(), map.get(cityWard).get(8) + list.get(i).getPopAgeW1(),
+                        map.get(cityWard).get(9) + sumAge2, map.get(cityWard).get(10) + list.get(i).getPopAgeM2(), map.get(cityWard).get(11) + list.get(i).getPopAgeW2(),
+                        map.get(cityWard).get(12) + sumAge3, map.get(cityWard).get(13) + list.get(i).getPopAgeM3(), map.get(cityWard).get(14) + list.get(i).getPopAgeW3(),
+                        map.get(cityWard).get(15) + sumAge4, map.get(cityWard).get(16) + list.get(i).getPopAgeM4(), map.get(cityWard).get(17) + list.get(i).getPopAgeW4(),
+                        map.get(cityWard).get(18) + sumAge5, map.get(cityWard).get(19) + list.get(i).getPopAgeM5(), map.get(cityWard).get(20) + list.get(i).getPopAgeW5(),
+                        map.get(cityWard).get(21) + sumAge6, map.get(cityWard).get(22) + list.get(i).getPopAgeM6(), map.get(cityWard).get(23) + list.get(i).getPopAgeW6(),
+                        map.get(cityWard).get(24) + sumAge7, map.get(cityWard).get(25) + list.get(i).getPopAgeM7(), map.get(cityWard).get(26) + list.get(i).getPopAgeW7(),
+                        map.get(cityWard).get(27) + sumAge8, map.get(cityWard).get(28) + list.get(i).getPopAgeM8(), map.get(cityWard).get(29) + list.get(i).getPopAgeW8(),
+                        map.get(cityWard).get(30) + sumAge9, map.get(cityWard).get(31) + list.get(i).getPopAgeM9(), map.get(cityWard).get(32) + list.get(i).getPopAgeW9()
                     	));
                 }
             }
@@ -232,39 +238,39 @@ public class PopulationMay0sRepository {
                         public void setValues(PreparedStatement ps, int i) throws SQLException {
                             ps.setLong(1, 1+i);
                             ps.setString(2, keyList.get(i));
-                            ps.setLong(3, 0L);
-                            ps.setLong(4, 0L);
-                            ps.setLong(5, 0L);
-                            ps.setLong(6, valueList.get(i).get(0));
-                            ps.setLong(7, valueList.get(i).get(1));
-                            ps.setLong(8, valueList.get(i).get(2));
-                            ps.setLong(9, valueList.get(i).get(3));
-                            ps.setLong(10, valueList.get(i).get(4));
-                            ps.setLong(11, valueList.get(i).get(5));
-                            ps.setLong(12, valueList.get(i).get(6));
-                            ps.setLong(13, valueList.get(i).get(7));
-                            ps.setLong(14, valueList.get(i).get(8));
-                            ps.setLong(15, valueList.get(i).get(9));
-                            ps.setLong(16, valueList.get(i).get(10));
-                            ps.setLong(17, valueList.get(i).get(11));
-                            ps.setLong(18, valueList.get(i).get(12));
-                            ps.setLong(19, valueList.get(i).get(13));
-                            ps.setLong(20, valueList.get(i).get(14));
-                            ps.setLong(21, valueList.get(i).get(15));
-                            ps.setLong(22, valueList.get(i).get(16));
-                            ps.setLong(23, valueList.get(i).get(17));
-                            ps.setLong(24, valueList.get(i).get(18));
-                            ps.setLong(25, valueList.get(i).get(19));
-                            ps.setLong(26, valueList.get(i).get(20));
-                            ps.setLong(27, valueList.get(i).get(21));
-                            ps.setLong(28, valueList.get(i).get(22));
-                            ps.setLong(29, valueList.get(i).get(23));
-                            ps.setLong(30, valueList.get(i).get(24));
-                            ps.setLong(31, valueList.get(i).get(25));
-                            ps.setLong(32, valueList.get(i).get(26));
-                            ps.setLong(33, valueList.get(i).get(27));
-                            ps.setLong(34, valueList.get(i).get(28));
-                            ps.setLong(35, valueList.get(i).get(29));
+                            ps.setLong(3, valueList.get(i).get(0));
+                            ps.setLong(4, valueList.get(i).get(1));
+                            ps.setLong(5, valueList.get(i).get(2));
+                            ps.setLong(6, valueList.get(i).get(3));
+                            ps.setLong(7, valueList.get(i).get(4));
+                            ps.setLong(8, valueList.get(i).get(5));
+                            ps.setLong(9, valueList.get(i).get(6));
+                            ps.setLong(10, valueList.get(i).get(7));
+                            ps.setLong(11, valueList.get(i).get(8));
+                            ps.setLong(12, valueList.get(i).get(9));
+                            ps.setLong(13, valueList.get(i).get(10));
+                            ps.setLong(14, valueList.get(i).get(11));
+                            ps.setLong(15, valueList.get(i).get(12));
+                            ps.setLong(16, valueList.get(i).get(13));
+                            ps.setLong(17, valueList.get(i).get(14));
+                            ps.setLong(18, valueList.get(i).get(15));
+                            ps.setLong(19, valueList.get(i).get(16));
+                            ps.setLong(20, valueList.get(i).get(17));
+                            ps.setLong(21, valueList.get(i).get(18));
+                            ps.setLong(22, valueList.get(i).get(19));
+                            ps.setLong(23, valueList.get(i).get(20));
+                            ps.setLong(24, valueList.get(i).get(21));
+                            ps.setLong(25, valueList.get(i).get(22));
+                            ps.setLong(26, valueList.get(i).get(23));
+                            ps.setLong(27, valueList.get(i).get(24));
+                            ps.setLong(28, valueList.get(i).get(25));
+                            ps.setLong(29, valueList.get(i).get(26));
+                            ps.setLong(30, valueList.get(i).get(27));
+                            ps.setLong(31, valueList.get(i).get(28));
+                            ps.setLong(32, valueList.get(i).get(29));
+                            ps.setLong(33, valueList.get(i).get(30));
+                            ps.setLong(34, valueList.get(i).get(31));
+                            ps.setLong(35, valueList.get(i).get(32));
                         }
     
                         @Override
