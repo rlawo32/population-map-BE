985b04de46bb0716752e5852be7098dc87a4e196
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-09-03 15:25:42 +0900

    [PopulationMap-project][BE] <DTO> 변수명_변경_작업_20240903 (3)

--- src/main/java/com/pm/pmproject/dto/Population10sDto.java
+++ src/main/java/com/pm/pmproject/dto/Population10sDto.java
@@ -12,64 +12,70 @@ public class Population10sDto {
 
     @JsonProperty("행정기관코드")
     private Long adminCode;
+
+    @JsonProperty("시도명")
+    private String nameCity;
+
+    @JsonProperty("시군구명")
+    private String nameWard;
   
     @JsonProperty("10세남자")
-    private Long popAgeM10;
+    private Long popAge10M;
 
     @JsonProperty("10세여자")
-    private Long popAgeW10;
+    private Long popAge10W;
 
     @JsonProperty("11세남자")
-    private Long popAgeM11;
+    private Long popAge11M;
 
     @JsonProperty("11세여자")
-    private Long popAgeW11;
+    private Long popAge11W;
   
     @JsonProperty("12세남자")
-    private Long popAgeM12;
+    private Long popAge12M;
 
     @JsonProperty("12세여자")
-    private Long popAgeW12;
+    private Long popAge12W;
   
     @JsonProperty("13세남자")
-    private Long popAgeM13;
+    private Long popAge13M;
 
     @JsonProperty("13세여자")
-    private Long popAgeW13;
+    private Long popAge13W;
 
     @JsonProperty("14세남자")
-    private Long popAgeM14;
+    private Long popAge14M;
 
     @JsonProperty("14세여자")
-    private Long popAgeW14;
+    private Long popAge14W;
 
     @JsonProperty("15세남자")
-    private Long popAgeM15;
+    private Long popAge15M;
 
     @JsonProperty("15세여자")
-    private Long popAgeW15;
+    private Long popAge15W;
 
     @JsonProperty("16세남자")
-    private Long popAgeM16;
+    private Long popAge16M;
 
     @JsonProperty("16세여자")
-    private Long popAgeW16;
+    private Long popAge16W;
 
     @JsonProperty("17세남자")
-    private Long popAgeM17;
+    private Long popAge17M;
 
     @JsonProperty("17세여자")
-    private Long popAgeW17;
+    private Long popAge17W;
 
     @JsonProperty("18세남자")
-    private Long popAgeM18;
+    private Long popAge18M;
 
     @JsonProperty("18세여자")
-    private Long popAgeW18;
+    private Long popAge18W;
   
     @JsonProperty("19세남자")
-    private Long popAgeM19;
+    private Long popAge19M;
 
     @JsonProperty("19세여자")
-    private Long popAgeW19;
+    private Long popAge19W;
 }
