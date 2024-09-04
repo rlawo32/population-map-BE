5ff45cda2a85be9c861261445727260b4796698c
rlawo <rlawo32@naver.com>
2024-09-05 02:06:58 +0900

    [PopulationMap-project][BE] <Domain> 컬럼명_변경_작업(6월)_20240905 (2)

--- src/main/java/com/pm/pmproject/jpa/domain/populationJun/PopulationJun20s.java
+++ src/main/java/com/pm/pmproject/jpa/domain/populationJun/PopulationJun20s.java
@@ -17,72 +17,72 @@ public class PopulationJun20s {
     @Column(name = "admin_code")
     private Long adminCode;
 
-    @Column(name = "pop_age_total")
-    private Long popAgeTotal;
+    @Column(name = "age_total")
+    private Long ageTotal;
 
-    @Column(name = "pop_age_m_total")
-    private Long popAgeMTotal;
+    @Column(name = "age_total_m")
+    private Long ageTotalM;
 
-    @Column(name = "pop_age_w_total")
-    private Long popAgeWTotal;
+    @Column(name = "age_total_w")
+    private Long ageTotalW;
 
-    @Column(name = "pop_age_m_20")
-    private Long popAgeM20;
+    @Column(name = "age_20_m")
+    private Long age20M;
 
-    @Column(name = "pop_age_w_20")
-    private Long popAgeW20;
+    @Column(name = "age_20_w")
+    private Long age20W;
 
-    @Column(name = "pop_age_m_21")
-    private Long popAgeM21;
+    @Column(name = "age_21_m")
+    private Long age21M;
 
-    @Column(name = "pop_age_w_21")
-    private Long popAgeW21;
+    @Column(name = "age_21_w")
+    private Long age21W;
 
-    @Column(name = "pop_age_m_22")
-    private Long popAgeM22;
+    @Column(name = "age_22_m")
+    private Long age22M;
 
-    @Column(name = "pop_age_w_22")
-    private Long popAgeW22;
+    @Column(name = "age_22_w")
+    private Long age22W;
 
-    @Column(name = "pop_age_m_23")
-    private Long popAgeM23;
+    @Column(name = "age_23_m")
+    private Long age23M;
 
-    @Column(name = "pop_age_w_23")
-    private Long popAgeW23;
+    @Column(name = "age_23_w")
+    private Long age23W;
 
-    @Column(name = "pop_age_m_24")
-    private Long popAgeM24;
+    @Column(name = "age_24_m")
+    private Long age24M;
 
-    @Column(name = "pop_age_w_24")
-    private Long popAgeW24;
+    @Column(name = "age_24_w")
+    private Long age24W;
 
-    @Column(name = "pop_age_m_25")
-    private Long popAgeM25;
+    @Column(name = "age_25_m")
+    private Long age25M;
 
-    @Column(name = "pop_age_w_25")
-    private Long popAgeW25;
+    @Column(name = "age_25_w")
+    private Long age25W;
 
-    @Column(name = "pop_age_m_26")
-    private Long popAgeM26;
+    @Column(name = "age_26_m")
+    private Long age26M;
 
-    @Column(name = "pop_age_w_26")
-    private Long popAgeW26;
+    @Column(name = "age_26_w")
+    private Long age26W;
 
-    @Column(name = "pop_age_m_27")
-    private Long popAgeM27;
+    @Column(name = "age_27_m")
+    private Long age27M;
 
-    @Column(name = "pop_age_w_27")
-    private Long popAgeW27;
+    @Column(name = "age_27_w")
+    private Long age27W;
 
-    @Column(name = "pop_age_m_28")
-    private Long popAgeM28;
+    @Column(name = "age_28_m")
+    private Long age28M;
 
-    @Column(name = "pop_age_w_28")
-    private Long popAgeW28;
+    @Column(name = "age_28_w")
+    private Long age28W;
 
-    @Column(name = "pop_age_m_29")
-    private Long popAgeM29;
+    @Column(name = "age_29_m")
+    private Long age29M;
 
-    @Column(name = "pop_age_w_29")
-    private Long popAgeW29;
+    @Column(name = "age_29_w")
+    private Long age29W;
 }
