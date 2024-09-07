74d1af7a0e904c39d72db433c95aecbdb6e5cff3
rlawo <rlawo32@naver.com>
2024-09-07 14:05:26 +0900

    [PopulationMap-project][BE] <JDBC> 지역별_TOTAL_작업(5월)_20240907 (11)

--- src/main/java/com/pm/pmproject/jpa/repository/populationMay/PopulationMay90sRepository.java
+++ src/main/java/com/pm/pmproject/jpa/repository/populationMay/PopulationMay90sRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class PopulationMay90sRepository {
