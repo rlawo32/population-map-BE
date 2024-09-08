80ed0426ada18a548bfd4026a1598fa32195603f
rlawo <rlawo32@naver.com>
2024-09-09 01:44:04 +0900

    [PopulationMap-project][BE] <Domain> TOTAL_테이블_추가_작업(11월)_20240909 (4)

---
+++ src/main/java/com/pm/pmproject/jpa/domain/populationNov/PopulationNovTotal100s.java
@@ -0,0 +1,112 @@
+package com.pm.pmproject.jpa.domain.populationNov;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "PopulationNov_total_100s")
+public class PopulationNovTotal100s {
+
+    @Id
+    @Column(name = "pop_seq")
+    private String popSeq;
+
+    @Column(name = "pop_place")
+    private String popPlace;
+
+    @Column(name = "age_100_total")
+    private Long age100Total;
+
+    @Column(name = "age_100_total_m")
+    private Long age100TotalM;
+
+    @Column(name = "age_100_total_w")
+    private Long age100TotalW;
+
+    @Column(name = "age_101_total")
+    private Long age101Total;
+
+    @Column(name = "age_101_total_m")
+    private Long age101TotalM;
+
+    @Column(name = "age_101_total_w")
+    private Long age101TotalW;
+
+    @Column(name = "age_102_total")
+    private Long age102Total;
+
+    @Column(name = "age_102_total_m")
+    private Long age102TotalM;
+
+    @Column(name = "age_102_total_w")
+    private Long age102TotalW;
+
+    @Column(name = "age_103_total")
+    private Long age103Total;
+
+    @Column(name = "age_103_total_m")
+    private Long age103TotalM;
+
+    @Column(name = "age_103_total_w")
+    private Long age103TotalW;
+
+    @Column(name = "age_104_total")
+    private Long age104Total;
+
+    @Column(name = "age_104_total_m")
+    private Long age104TotalM;
+
+    @Column(name = "age_104_total_w")
+    private Long age104TotalW;
+
+    @Column(name = "age_105_total")
+    private Long age105Total;
+
+    @Column(name = "age_105_total_m")
+    private Long age105TotalM;
+
+    @Column(name = "age_105_total_w")
+    private Long age105TotalW;
+
+    @Column(name = "age_106_total")
+    private Long age106Total;
+
+    @Column(name = "age_106_total_m")
+    private Long age106TotalM;
+
+    @Column(name = "age_106_total_w")
+    private Long age106TotalW;
+
+    @Column(name = "age_107_total")
+    private Long age107Total;
+
+    @Column(name = "age_107_total_m")
+    private Long age107TotalM;
+
+    @Column(name = "age_107_total_w")
+    private Long age107TotalW;
+
+    @Column(name = "age_108_total")
+    private Long age108Total;
+
+    @Column(name = "age_108_total_m")
+    private Long age108TotalM;
+
+    @Column(name = "age_108_total_w")
+    private Long age108TotalW;
+
+    @Column(name = "age_109_total")
+    private Long age109Total;
+
+    @Column(name = "age_109_total_m")
+    private Long age109TotalM;
+
+    @Column(name = "age_109_total_w")
+    private Long age109TotalW;
+}
---
+++ src/main/java/com/pm/pmproject/jpa/domain/populationNov/PopulationNovTotal80s.java
@@ -0,0 +1,112 @@
+package com.pm.pmproject.jpa.domain.populationNov;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "PopulationNov_total_80s")
+public class PopulationNovTotal80s {
+
+    @Id
+    @Column(name = "pop_seq")
+    private String popSeq;
+
+    @Column(name = "pop_place")
+    private String popPlace;
+
+    @Column(name = "age_80_total")
+    private Long age80Total;
+
+    @Column(name = "age_80_total_m")
+    private Long age80TotalM;
+
+    @Column(name = "age_80_total_w")
+    private Long age80TotalW;
+
+    @Column(name = "age_81_total")
+    private Long age81Total;
+
+    @Column(name = "age_81_total_m")
+    private Long age81TotalM;
+
+    @Column(name = "age_81_total_w")
+    private Long age81TotalW;
+
+    @Column(name = "age_82_total")
+    private Long age82Total;
+
+    @Column(name = "age_82_total_m")
+    private Long age82TotalM;
+
+    @Column(name = "age_82_total_w")
+    private Long age82TotalW;
+
+    @Column(name = "age_83_total")
+    private Long age83Total;
+
+    @Column(name = "age_83_total_m")
+    private Long age83TotalM;
+
+    @Column(name = "age_83_total_w")
+    private Long age83TotalW;
+
+    @Column(name = "age_84_total")
+    private Long age84Total;
+
+    @Column(name = "age_84_total_m")
+    private Long age84TotalM;
+
+    @Column(name = "age_84_total_w")
+    private Long age84TotalW;
+
+    @Column(name = "age_85_total")
+    private Long age85Total;
+
+    @Column(name = "age_85_total_m")
+    private Long age85TotalM;
+
+    @Column(name = "age_85_total_w")
+    private Long age85TotalW;
+
+    @Column(name = "age_86_total")
+    private Long age86Total;
+
+    @Column(name = "age_86_total_m")
+    private Long age86TotalM;
+
+    @Column(name = "age_86_total_w")
+    private Long age86TotalW;
+
+    @Column(name = "age_87_total")
+    private Long age87Total;
+
+    @Column(name = "age_87_total_m")
+    private Long age87TotalM;
+
+    @Column(name = "age_87_total_w")
+    private Long age87TotalW;
+
+    @Column(name = "age_88_total")
+    private Long age88Total;
+
+    @Column(name = "age_88_total_m")
+    private Long age88TotalM;
+
+    @Column(name = "age_88_total_w")
+    private Long age88TotalW;
+
+    @Column(name = "age_89_total")
+    private Long age89Total;
+
+    @Column(name = "age_89_total_m")
+    private Long age89TotalM;
+
+    @Column(name = "age_89_total_w")
+    private Long age89TotalW;
+}
